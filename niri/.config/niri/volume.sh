c109269e2d3773be4dcb7cb7d3e66956711ae4d2
BotNikos <boss.nikitos1212@mail.ru>
2025-12-17 16:38:37 +0300

    Delete shell volume script (rewritten in scheme for now)

--- niri/.config/niri/volume.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Get the volume level and convert it to a percentage
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-volume=$(echo "$volume" | awk '{print $2}')
-volumePercent=$(echo "( $volume * 100 ) / 1" | bc)
-volume=$(echo "( $volume * 10 ) / 1" | bc)
-
-notify-send -t 2000 -a 'wp-vol' -h int:value:$volumePercent " ${volume}/10"
